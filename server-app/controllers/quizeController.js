034f5e189e33bb7a75d031477719f83fe3949f11
sandeshpatil2299 <patilsandesh2299@gmail.com>
2026-02-24 00:41:12 +0530

    Implemented quizzes routes

---
+++ server-app/controllers/quizeController.js
@@ -0,0 +1,13 @@
+import Quiz from "../models/Quiz.js";
+
+//@desc Get all quizzes for a document
+//@route GEt /api/quizzes/:documentId
+//@access Private
+
+const getQuizzes= async (req, res, next) => {
+    try {
+        
+    } catch (error) {
+        next(error);
+    }
+}
---
+++ server-app/routes/quizeRoutes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import protect from '../middleware/auth.js';
+import {
+    getQuizzes,
+    getQuizById,
+    submitQuiz,
+    getQuizResults,
+    deleteQuiz
+} from '../controllers/quizeController.js'
+
+const router= express.Router();
+
+router.use(protect);
+
+router.get('/:documentId', getQuizzes);
+router.get('/quiz/:id', getQuizById);
+router.post('/:id/submit', submitQuiz);
+router.get('/:id/results', getQuizResults);
+router.delete('/:id', deleteQuiz);
+
+export default router;
